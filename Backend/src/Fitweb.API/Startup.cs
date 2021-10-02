0f52ee3f48cc1cb1fbd87a02aa3a08d09c321b1a
karolNiedziela <imniedziel@gmail.com>
2021-10-02 16:48:09 +0200

    Add information about request duration to Swagger UI

--- Backend/src/Fitweb.API/Startup.cs
+++ Backend/src/Fitweb.API/Startup.cs
@@ -122,7 +122,11 @@ namespace Fitweb.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fitweb.API v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fitweb.API v1");
+                    c.DisplayRequestDuration();
+                });
             }
 
             app.UseCors("FitwebOrigin");
