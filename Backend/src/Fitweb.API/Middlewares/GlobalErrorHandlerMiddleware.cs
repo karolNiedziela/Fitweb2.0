861bb24373cececd09de4d9208bc6fa20c71ee48
karolNiedziela <imniedziel@gmail.com>
2021-10-03 11:44:28 +0200

    Add warning logs

--- Backend/src/Fitweb.API/Middlewares/GlobalErrorHandlerMiddleware.cs
+++ Backend/src/Fitweb.API/Middlewares/GlobalErrorHandlerMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Fitweb.Infrastructure.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,12 @@ namespace Fitweb.API.Middlewares
     public class GlobalErrorHandlerMiddleware : IMiddleware
     {
         private readonly IExceptionToErrorResponseMapper _exceptionMapper;
+        private readonly ILogger<GlobalErrorHandlerMiddleware> _logger;
 
-        public GlobalErrorHandlerMiddleware(IExceptionToErrorResponseMapper exceptionMapper)
+        public GlobalErrorHandlerMiddleware(IExceptionToErrorResponseMapper exceptionMapper, ILogger<GlobalErrorHandlerMiddleware> logger)
         {
             _exceptionMapper = exceptionMapper;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
@@ -30,8 +33,9 @@ namespace Fitweb.API.Middlewares
             }
         }
 
-        private async Task HandleErrorAsync(HttpContext context, Exception  exception)
+        private async Task HandleErrorAsync(HttpContext context, Exception exception)
         {
+            _logger.LogWarning($"{exception.Source} : { exception.Message}");
             var errorResponse = _exceptionMapper.Map(exception);
             context.Response.StatusCode = (int)(errorResponse?.StatusCode ?? HttpStatusCode.InternalServerError);
 
