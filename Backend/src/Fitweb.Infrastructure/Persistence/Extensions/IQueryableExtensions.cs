24f3f2f8ea5f6845033fe41ab18e6a4fbc2e4f54
karolNiedziela <imniedziel@gmail.com>
2021-10-05 21:01:39 +0200

    Comment unused code, violate YAGNI

--- Backend/src/Fitweb.Infrastructure/Persistence/Extensions/IQueryableExtensions.cs
+++ Backend/src/Fitweb.Infrastructure/Persistence/Extensions/IQueryableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fitweb.Domain.Filters;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,31 +11,31 @@ namespace Fitweb.Infrastructure.Persistence.Extensions
 {
     public static class IQueryableExtensions
     {
-        public static IQueryable<T> Where<T>(this IQueryable<T> source, string columnName, string comparison, object value)
-        {
-            if (string.IsNullOrEmpty(columnName))
-            {
-                return source;
-            }
+        //public static IQueryable<T> Where<T>(this IQueryable<T> source, string columnName, string comparison, object value)
+        //{
+        //    if (string.IsNullOrEmpty(columnName))
+        //    {
+        //        return source;
+        //    }
 
-            var parameter = Expression.Parameter(typeof(T), "x");
+        //    var parameter = Expression.Parameter(typeof(T), "x");
 
-            var property = columnName.Split('.')
-                                .Aggregate<string, Expression>
-                                    (parameter, (c, m) => Expression.Property(c, m));
+        //    var property = columnName.Split('.')
+        //                        .Aggregate<string, Expression>
+        //                            (parameter, (c, m) => Expression.Property(c, m));
 
-            var body = MakeComparison(property, comparison, value);
+        //    var body = MakeComparison(property, comparison, value);
 
-            var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+        //    var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
 
-            var quote = Expression.Quote(lambda);
+        //    var quote = Expression.Quote(lambda);
 
-            var methodCallExpression = Expression.Call(typeof(Queryable), "Where",
-                            new Type[] { source.ElementType },
-                            source.Expression, quote);
+        //    var methodCallExpression = Expression.Call(typeof(Queryable), "Where",
+        //                    new Type[] { source.ElementType },
+        //                    source.Expression, quote);
 
-            return source.Provider.CreateQuery<T>(methodCallExpression);
-        }
+        //    return source.Provider.CreateQuery<T>(methodCallExpression);
+        //}
 
         //https://stackoverflow.com/questions/29084894/how-to-use-an-expressionfunc-to-set-a-nested-property
         public static IQueryable<T> ApplyOrderBy<T>(this IQueryable<T> source, string columnName, bool isAscending)
@@ -64,34 +65,34 @@ namespace Fitweb.Infrastructure.Persistence.Extensions
                         .ToListAsync();
         }
 
-        private static Expression MakeComparison(Expression left, string comparison, object value)
-        {
-            var constant = Expression.Constant(value, left.Type);
-            switch (comparison)
-            {
-                case "==":
-                    return Expression.MakeBinary(ExpressionType.Equal, left, constant);
-                case "!=":
-                    return Expression.MakeBinary(ExpressionType.NotEqual, left, constant);
-                case ">":
-                    return Expression.MakeBinary(ExpressionType.GreaterThan, left, constant);
-                case ">=":
-                    return Expression.MakeBinary(ExpressionType.GreaterThanOrEqual, left, constant);
-                case "<":
-                    return Expression.MakeBinary(ExpressionType.LessThan, left, constant);
-                case "<=":
-                    return Expression.MakeBinary(ExpressionType.LessThanOrEqual, left, constant);
-                case "Contains":
-                case "StartsWith":
-                case "EndsWith":
-                    if (value is string)
-                        return Expression.Call(left, comparison, Type.EmptyTypes, Expression.Constant(value, typeof(string)));
+        //private static Expression MakeComparison(Expression left, string comparison, object value)
+        //{
+        //    var constant = Expression.Constant(value, left.Type);
+        //    switch (comparison)
+        //    {
+        //        case "==":
+        //            return Expression.MakeBinary(ExpressionType.Equal, left, constant);
+        //        case "!=":
+        //            return Expression.MakeBinary(ExpressionType.NotEqual, left, constant);
+        //        case ">":
+        //            return Expression.MakeBinary(ExpressionType.GreaterThan, left, constant);
+        //        case ">=":
+        //            return Expression.MakeBinary(ExpressionType.GreaterThanOrEqual, left, constant);
+        //        case "<":
+        //            return Expression.MakeBinary(ExpressionType.LessThan, left, constant);
+        //        case "<=":
+        //            return Expression.MakeBinary(ExpressionType.LessThanOrEqual, left, constant);
+        //        case "Contains":
+        //        case "StartsWith":
+        //        case "EndsWith":
+        //            if (value is string)
+        //                return Expression.Call(left, comparison, Type.EmptyTypes, Expression.Constant(value, typeof(string)));
 
-                    throw new NotSupportedException($"Invalid comparison operator '{comparison}'.");
-                default:
-                    throw new NotSupportedException($"Invalid comparison operator '{comparison}'.");
-            }
-        }
+        //            throw new NotSupportedException($"Invalid comparison operator '{comparison}'.");
+        //        default:
+        //            throw new NotSupportedException($"Invalid comparison operator '{comparison}'.");
+        //    }
+        //}
     }
 
 }
