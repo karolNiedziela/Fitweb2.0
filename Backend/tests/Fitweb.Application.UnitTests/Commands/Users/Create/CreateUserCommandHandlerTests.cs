4a3eee287af44e98b439ba6115f29d0845d433b9
karolNiedziela <imniedziel@gmail.com>
2021-10-10 15:20:04 +0200

    Remove not needed tests

--- Backend/tests/Fitweb.Application.UnitTests/Commands/Users/Create/CreateUserCommandHandlerTests.cs
+++ Backend/tests/Fitweb.Application.UnitTests/Commands/Users/Create/CreateUserCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Fitweb.Application.Commands.Users.Create;
 using Fitweb.Application.Interfaces.Identity;
 using Fitweb.Application.Responses;
+using Fitweb.Domain.Athletes.Repositories;
 using FluentAssertions;
 using MediatR;
 using NSubstitute;
@@ -14,24 +15,30 @@ namespace Fitweb.Application.UnitTests.Commands.Users.Create
     public class CreateUserCommandHandlerTests
     {
         private readonly IIdentityService _identityService;
+        private readonly IAthleteRepository _athleteRepository;
+        private readonly RegisterUserCommandHandler _sut;
+
         public CreateUserCommandHandlerTests()
         {
             _identityService = Substitute.For<IIdentityService>();
+            _athleteRepository = Substitute.For<IAthleteRepository>();
+            _sut = new RegisterUserCommandHandler(_identityService, _athleteRepository);
         }
 
         [Fact]
         public async Task CreateUser_ShouldAddNewUser()
         {
-            var handler = new CreateUserCommandHandler(_identityService);
-
-            var command = new CreateUserCommand
+            var command = new RegisterUserCommand
             {
                 Username = "testUser",
                 Email = "testUser@email.com",
                 Password = "testSecret1="
             };
 
-            var result = await handler.Handle(command, CancellationToken.None);
+            var userId = "user_id";
+            _identityService.CreateUserAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(userId);
+
+            var result = await _sut.Handle(command);
 
             result.Should().BeOfType<Response<string>>();
             result.Message.Should().Be("User added successfully.");
